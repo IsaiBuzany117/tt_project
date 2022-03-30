ea94a6faba4f56417274889bfc04b7bb00c38053
Isai Buzany <isaibuzany117@gmail.com>
2022-03-29 21:00:55 -0600

    agregando primer parte de inicio

--- components/navinicio.js
+++ components/navinicio.js
@@ -3,7 +3,7 @@ import Link from "next/link"
 const Navinicio = () => {
   return (
     <nav className="flex justify-center">
-      <div className="grid grid-cols-2 translate-y-2 w-full mx-8 bg-slate-100 border-2 rounded-md">
+      <div className="grid grid-cols-2 translate-y-2 w-full mx-8 bg-slate-100 border-2 border-slate-300 rounded-md">
         <div className="flex">
           <span className="mx-2 my-1 font-bold cursor-pointer transition duration-150 ease-in-out hover:scale-105  hover:text-purple-800 hover:underline">Busqueda</span>
         </div>
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,20 @@
 import Navinicio from "../components/navinicio"
+import Image from "next/image"
+import logo1 from "../assets/logo1.svg"
 
 const index = () => {
   return (
     <div className='h-screen w-full'>
-      <div className='w-full h-3/4 bg-purple-700'>
-        <Navinicio />
-      </div>
       <div className='w-full h-3/4 bg-slate-100'>
+        <Navinicio />
+        <div className="flex justify-center w-full mt-8">
+          <Image src={logo1} alt="logo"/>  
+        </div>
+      </div>
+      <div className='w-full h-3/4 bg-purple-700'>
 
       </div>
-      <div className='w-full h-3/4 bg-purple-700'>
+      <div className='w-full h-3/4 bg-slate-100'>
 
       </div>
     </div>
