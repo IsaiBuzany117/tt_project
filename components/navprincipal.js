066b351d7b3f2d423447661fd2c3a8286bb27a3b
Isai Buzany <isaibuzany117@gmail.com>
2022-04-21 13:44:16 -0500

    agregando icono notificacion

--- components/navprincipal.js
+++ components/navprincipal.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Link from 'next/link'
+import { MdNotifications } from 'react-icons/md'
 
 const NavbarP = () => {
     return (
@@ -11,6 +12,9 @@ const NavbarP = () => {
                         <span className='text-center mx-3 my-1 font-bold cursor-pointer transition duration-150 ease-in-out hover:scale-105  hover:text-purple-800 hover:underline'>Inicio</span>
                     </Link>
                     <span className='text-center mx-3 my-1 font-bold cursor-pointer transition duration-150 ease-in-out hover:scale-105  hover:text-purple-800 hover:underline'>Mi cuenta</span>
+                    <span className='mx-3 my-1 cursor-pointer transition duration-150 ease-in-out hover:scale-105  hover:text-purple-800'>
+                        <MdNotifications className="w-6 h-6"/>
+                    </span>
                 </div>
             </div>
         </div>
