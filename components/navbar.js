b379e963e6c3c8b39677761bf03824e032ef5b21
Isai Buzany <isaibuzany117@gmail.com>
2022-04-22 12:36:09 -0500

    agregando redirecciones a navbar

--- components/navbar.js
+++ components/navbar.js
@@ -11,8 +11,12 @@ const Navbar = () => {
                     <Link href='/' passHref>
                         <span className='text-center mx-1 my-1 font-bold cursor-pointer transition duration-150 ease-in-out hover:scale-105  hover:text-purple-800 hover:underline'>Inicio</span>
                     </Link>
-                    <span className='text-center mx-1 my-1 font-bold cursor-pointer transition duration-150 ease-in-out hover:scale-105  hover:text-purple-800 hover:underline'>Soy Paciente</span>
-                    <span className='text-center mx-1 my-1 font-bold cursor-pointer transition duration-150 ease-in-out hover:scale-105  hover:text-purple-800 hover:underline'>Soy Medico</span>
+                    <Link href="/registro/paciente" passHref>
+                        <span className='text-center mx-1 my-1 font-bold cursor-pointer transition duration-150 ease-in-out hover:scale-105  hover:text-purple-800 hover:underline'>Soy Paciente</span>
+                    </Link>
+                    <Link href="/registro/medico" passHref>
+                        <span className='text-center mx-1 my-1 font-bold cursor-pointer transition duration-150 ease-in-out hover:scale-105  hover:text-purple-800 hover:underline'>Soy Medico</span>
+                    </Link>
                 </div>
             </div>
         </div>
